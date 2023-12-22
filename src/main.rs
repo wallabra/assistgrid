0479e807a45d1b053f5cf9cc3d26c25135c15f97
Gustavo Rehermann <rehermann6046@gmail.com>
2023-12-22 15:08:12 -0300

    Initialize ItemTypes in main

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,10 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(
-            Startup,
-            (
-                initial_item_types,
-                initial_friction::<{ (0.7 * 128.0) as i64 }>,
-            )
-                .before(initial_people),
-        )
-        .add_systems(Startup, initial_people)
+        //.add_startup_systems(initial_item_types)
+        .init_resource::<assistgrid::item::ItemTypes>()
+        .add_systems(Startup, initial_friction::<{ (0.7 * 128.0) as i64 }>)
+        .add_systems(Startup, initial_people.after(initial_item_types))
         .add_systems(Update, (hello_world_system, update_movement))
         .run();
 }
